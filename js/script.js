37d922181beec35d97f0587100de1c26f01ab786
Lukasz Smiechowski <lukasz.smiechowski@me.com>
2023-08-07 19:53:20 +0200

    dodanie Julity

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const btnNav = document.querySelector('.btn-mobile-nav');
 const headerEl = document.querySelector('.header');
 
 btnNav.addEventListener('click', function () {
+  
   headerEl.classList.toggle('nav-open');
 });
 
